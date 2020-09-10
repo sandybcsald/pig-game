df869272807098bb2a8531186e7baee58d5d459a
adityarungta2048 <71099075+AdityaRungta2048@users.noreply.github.com>
2020-09-11 03:37:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
     
     //Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
